8340f63dee2869286fa77ecb0f16dceaa1451534
GeraldJones <gajone12@asu.edu>
2024-03-08 14:56:43 -0800

    feat: Add operate function to perform the chosen operation on the user defined numbers

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let num1 = 0;
 let op;
+let num1 = 0;
 let num2 = 0;
 
 
@@ -17,4 +17,18 @@ function multiply(a, b) {
 
 function divide(a, b) {
     return b == 0 ? "undefined" : a / b;
+}
+
+function operate(op, num1, num2) {
+    if (op === '+') {
+        return add(num1, num2);
+    } else if (op === '-') {
+        return subtract(num1, num2);
+    } else if (op === '*') {
+        return multiply(num1, num2);
+    } else if (op === "/") {
+        return divide(num1, num2);
+    } else {
+        return "bad sign";
+    }
 }
