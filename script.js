410e8c0d1449698e75626498b0e311dfde3df9f7
GeraldJones <gajone12@asu.edu>
2024-03-08 15:13:04 -0800

    feat: Create a base calculator display of 4x5. Add dummy numbers in sqaures

--- script.js
+++ script.js
@@ -1,20 +1,43 @@
+/* Starter Variables */
+const containerDiv = document.querySelector(".container");
+const row = document.getElementsByClassName('row');
+const square = document.getElementsByClassName('square');
 let op;
 let num1 = 0;
 let num2 = 0;
 
+/* Initialize Grid */
+row.textContent = 5;
+square.textContent = 4;
+makeGrid();
+
+function makeGrid() {
+    for (let i = 1; i <= row.textContent; i++) {
+        const rowDiv = document.createElement("div");
+        rowDiv.setAttribute('class', 'row');
+        containerDiv.append(rowDiv);
+
+        for (let j = 1; j <= square.textContent; j++) {
+            const squareDiv = document.createElement("div");
+            squareDiv.setAttribute('class', 'square');
+            squareDiv.style.height = `${600 / row.textContent}px`;
+            squareDiv.style.width = `${600 / square.textContent}px`;
+            squareDiv.textContent = '0';
+            squareDiv.style.fontSize = "24px";
+            rowDiv.append(squareDiv);
+        }
+    }
+}
 
 function add(a, b) {
     return a + b;
 }
-
 function subtract(a, b) {
     return a - b;
 }
-
 function multiply(a, b) {
     return a * b;
 }
-
 function divide(a, b) {
     return b == 0 ? "undefined" : a / b;
 }
